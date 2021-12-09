308bbd4081978e8e7308758adfebeb50f1f8a1a5
aabdugal <aabdugal@gmail.com>
2021-12-09 04:52:32 -0500

    heroku fix

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ require('./app/routes/post.routes.js')(app)
 require('./app/routes/requests.routes.js')(app)
 // require('./app/routes/trips.routes.js')(app)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
